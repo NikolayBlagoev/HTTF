d394ba1f910ca9b94b39a36ac206d74e40a95eb3
NikolayBlagoev <workerthreadllc@gmail.com>
2025-12-01 14:04:20 +0200

    log

--- src/train_mean.py
+++ src/train_mean.py
@@ -51,6 +51,7 @@ for params in model.model.embed_tokens.parameters():
 for idx in range(28):
     if idx % 4 != 3:
         continue
+    print("layer",idx,"FROZEN")
     for params in model.model.layers[idx].parameters():
         params.requires_grad_(False)
         params.requires_grad  = False
